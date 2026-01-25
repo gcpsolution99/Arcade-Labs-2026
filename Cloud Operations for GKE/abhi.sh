042f11f3c03ee95d365826dd20918e309343b1a4
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2026-01-25 12:10:25 +0530

    Create abhi.sh

---
+++ Cloud Operations for GKE/abhi.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+YELLOW='\033[0;33m'
+NC='\033[0m' 
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
+read -p "Enter the ZONE: " ZONE
+export ZONE
+gcloud auth list
+export PROJECT_ID=$(gcloud config get-value project)
+export REGION=${ZONE%-*}
+gcloud config set compute/region $REGION
+gcloud config set compute/zone $ZONE
+
+gsutil cp gs://spls/gsp497/gke-monitoring-tutorial.zip .
+unzip gke-monitoring-tutorial.zip
+
+cd gke-monitoring-tutorial
+make create
+make validate
+make teardown
+
+pattern=(
+"**********************************************************"
+"**                 S U B S C R I B E  TO                **"
+"**                 ABHI ARCADE SOLUTION                 **"
+"**                                                      **"
+"**********************************************************"
+)
+for line in "${pattern[@]}"
+do
+    echo -e "${YELLOW}${line}${NC}"
+done
