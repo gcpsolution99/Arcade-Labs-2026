925cb5bf252c045308077b1fdb338401781788e4
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2026-02-13 19:17:41 +0530

    Update abhi.sh

--- Rate Limiting with Cloud Armor/abhi.sh
+++ Rate Limiting with Cloud Armor/abhi.sh
@@ -23,30 +23,22 @@ PROJECT_ID=`gcloud config get-value project`
 export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
 
 gcloud services enable osconfig.googleapis.com > /dev/null 2>&1 &
-spinner
 
 gcloud compute --project=$PROJECT_ID firewall-rules create default-allow-http --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=http-server > /dev/null 2>&1 &
-spinner
 
 gcloud compute --project=$PROJECT_ID firewall-rules create default-allow-health-check --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 --source-ranges=130.211.0.0/22,35.191.0.0/16 --target-tags=http-server > /dev/null 2>&1 &
-spinner
 
 gcloud compute instance-templates create $REGION1-template --project=$PROJECT_ID --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=startup-script-url=gs://cloud-training/gcpnet/httplb/startup.sh,enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append --region=$REGION1 --tags=http-server --create-disk=auto-delete=yes,boot=yes,device-name=$REGION1-template,image=projects/debian-cloud/global/images/debian-12-bookworm-v20250311,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any > /dev/null 2>&1 &
-spinner
 
 gcloud compute instance-templates create $REGION2-template --project=$PROJECT_ID --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=startup-script-url=gs://cloud-training/gcpnet/httplb/startup.sh,enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append --region=$REGION2 --tags=http-server --create-disk=auto-delete=yes,boot=yes,device-name=$REGION2-template,image=projects/debian-cloud/global/images/debian-12-bookworm-v20250311,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any > /dev/null 2>&1 &
-spinner
 
 gcloud beta compute instance-groups managed create $REGION1-mig --project=$PROJECT_ID --base-instance-name=$REGION1-mig --template=projects/$PROJECT_ID/global/instanceTemplates/$REGION1-template --size=1 --region=$REGION1 --target-distribution-shape=EVEN --instance-redistribution-type=proactive --default-action-on-vm-failure=repair --no-force-update-on-repair --standby-policy-mode=manual --list-managed-instances-results=pageless > /dev/null 2>&1 &
-spinner
 gcloud beta compute instance-groups managed set-autoscaling $REGION1-mig --project=$PROJECT_ID --region=$REGION1 --mode=on --min-num-replicas=1 --max-num-replicas=5 --target-cpu-utilization=0.8 --cpu-utilization-predictive-method=none --cool-down-period=45 > /dev/null 2>&1 &
-spinner
-
 
 gcloud beta compute instance-groups managed create $REGION2-mig --project=$PROJECT_ID --base-instance-name=$REGION2-mig --template=projects/$PROJECT_ID/global/instanceTemplates/$REGION2-template --size=1 --region=$REGION2 --target-distribution-shape=EVEN --instance-redistribution-type=proactive --default-action-on-vm-failure=repair --no-force-update-on-repair --standby-policy-mode=manual --list-managed-instances-results=pageless > /dev/null 2>&1 &
-spinner
+
 gcloud beta compute instance-groups managed set-autoscaling $REGION2-mig --project=$PROJECT_ID --region=$REGION2 --mode=on --min-num-replicas=1 --max-num-replicas=5 --target-cpu-utilization=0.8 --cpu-utilization-predictive-method=none --cool-down-period=45 > /dev/null 2>&1 &
-spinner
+
 
 token=$(gcloud auth application-default print-access-token)
 project_id=$(gcloud config get-value project)
@@ -66,7 +58,6 @@ curl -X POST -H "Content-Type: application/json" \
     "unhealthyThreshold": 2
   }' \
   "https://compute.googleapis.com/compute/beta/projects/$project_id/global/healthChecks" > /dev/null 2>&1 &
-spinner
 
 for i in $(seq 30 -1 1); do
   echo -ne "${YELLOW_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
@@ -95,7 +86,6 @@ curl -X POST -H "Content-Type: application/json" \
     ]
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$project_id/global/securityPolicies" > /dev/null 2>&1 &
-spinner
 
 for i in $(seq 30 -1 1); do
   echo -ne "${YELLOW_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
@@ -121,7 +111,6 @@ curl -X POST -H "Content-Type: application/json" \
     "timeoutSec": 30
   }' \
   "https://compute.googleapis.com/compute/beta/projects/$project_id/global/backendServices" > /dev/null 2>&1 &
-spinner
 
 for i in $(seq 60 -1 1); do
   echo -ne "${YELLOW_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
@@ -136,7 +125,6 @@ curl -X POST -H "Content-Type: application/json" \
     "name": "http-lb"
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$project_id/global/urlMaps" > /dev/null 2>&1 &
-spinner
 
 for i in $(seq 30 -1 1); do
   echo -ne "${YELLOW_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
@@ -151,7 +139,6 @@ curl -X POST -H "Content-Type: application/json" \
     "urlMap": "projects/'"$project_id"'/global/urlMaps/http-lb"
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$project_id/global/targetHttpProxies" > /dev/null 2>&1 &
-spinner
 
 curl -X POST -H "Content-Type: application/json" \
   -H "Authorization: Bearer $token" \
@@ -160,7 +147,6 @@ curl -X POST -H "Content-Type: application/json" \
     "urlMap": "projects/'"$project_id"'/global/urlMaps/http-lb"
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$project_id/global/targetHttpProxies" > /dev/null 2>&1 &
-spinner
 
 for i in $(seq 30 -1 1); do
   echo -ne "${YELLOW_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
@@ -179,7 +165,6 @@ curl -X POST -H "Content-Type: application/json" \
     "target": "projects/'"$project_id"'/global/targetHttpProxies/http-lb-target-proxy"
   }' \
   "https://compute.googleapis.com/compute/beta/projects/$project_id/global/forwardingRules" > /dev/null 2>&1 &
-spinner
 
 for i in $(seq 20 -1 1); do
   echo -ne "${YELLOW_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
@@ -199,7 +184,6 @@ curl -X POST -H "Content-Type: application/json" \
     "target": "projects/'"$project_id"'/global/targetHttpProxies/http-lb-target-proxy-2"
   }' \
   "https://compute.googleapis.com/compute/beta/projects/$project_id/global/forwardingRules" > /dev/null 2>&1 &
-spinner
 
 for i in $(seq 20 -1 1); do
   echo -ne "${YELLOW_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
@@ -211,29 +195,24 @@ curl -X POST -H "Content-Type: application/json" \
   -H "Authorization: Bearer $token" \
   -d '{"namedPorts": [{"name": "http", "port": 80}]}' \
   "https://compute.googleapis.com/compute/beta/projects/$project_id/regions/$REGION1/instanceGroups/$REGION1-mig/setNamedPorts" > /dev/null 2>&1 &
-spinner
 
 curl -X POST -H "Content-Type: application/json" \
   -H "Authorization: Bearer $token" \
   -d '{"namedPorts": [{"name": "http", "port": 80}]}' \
   "https://compute.googleapis.com/compute/beta/projects/$project_id/regions/$REGION2/instanceGroups/$REGION2-mig/setNamedPorts" > /dev/null 2>&1 &
-spinner
 
 gcloud compute instances create siege-vm --project=$PROJECT_ID --zone=$ZONE3 --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-osconfig=TRUE,enable-oslogin=false --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append --create-disk=auto-delete=yes,boot=yes,device-name=siege-vm,image=projects/debian-cloud/global/images/debian-12-bookworm-v20250311,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ops-agent-policy=v2-x86-template-1-4-0,goog-ec-src=vm_add-gcloud --reservation-affinity=any > /dev/null 2>&1 &
-spinner
+
 printf 'agentsRule:\n  packageState: installed\n  version: latest\ninstanceFilter:\n  inclusionLabels:\n  - labels:\n      goog-ops-agent-policy: v2-x86-template-1-4-0\n' > config.yaml && gcloud compute instances ops-agents policies create goog-ops-agent-v2-x86-template-1-4-0-$ZONE3 --project=$PROJECT_ID --zone=$ZONE3 --file=config.yaml > /dev/null 2>&1 &
-spinner
+
 gcloud compute resource-policies create snapshot-schedule default-schedule-1 --project=$PROJECT_ID --region=$REGION3 --max-retention-days=14 --on-source-disk-delete=keep-auto-snapshots --daily-schedule --start-time=16:00 > /dev/null 2>&1 &
-spinner
+
 gcloud compute disks add-resource-policies siege-vm --project=$PROJECT_ID --zone=$ZONE3 --resource-policies=projects/$PROJECT_ID/regions/$REGION3/resourcePolicies/default-schedule-1 > /dev/null 2>&1 &
-spinner
 
 gcloud compute ssh --zone "$ZONE3" "siege-vm" --project "$PROJECT_ID" --command "sudo apt-get -y install siege" --quiet > /dev/null 2>&1 &
-spinner
 
 gcloud compute security-policies create rate-limit-siege \
     --description "policy for rate limiting" > /dev/null 2>&1 &
-spinner
 
 gcloud beta compute security-policies rules create 100 \
     --security-policy=rate-limit-siege \
@@ -245,11 +224,9 @@ gcloud beta compute security-policies rules create 100 \
     --conform-action=allow \
     --exceed-action=deny-404 \
     --enforce-on-key=IP > /dev/null 2>&1 &
-spinner
 
 gcloud compute backend-services update http-backend \
     --security-policy rate-limit-siege --global > /dev/null 2>&1 &
-spinner
 
 pattern=(
 "**********************************************************"
