b18f9bc79ac2a1feb5224e552032675798d17d19
Abhi Arcade Solution <69536662+gcpsolution99@users.noreply.github.com>
2026-02-13 20:08:42 +0530

    Update abhi.sh

--- Cloud DNS: Traffic Steering using Geolocation Policy/abhi.sh
+++ Cloud DNS: Traffic Steering using Geolocation Policy/abhi.sh
@@ -22,7 +22,7 @@ read -p "Enter Zone 3: " zone3
 gcloud auth list
 gcloud config list project
 
-gcloud services enable compute.googleapis.com dns.googleapis.com >/dev/null 2>&1 &
+gcloud services enable compute.googleapis.com dns.googleapis.com >/dev/null 2>&1
 
 gcloud compute firewall-rules create fw-default-iapproxy \
 --direction=INGRESS \
@@ -30,7 +30,7 @@ gcloud compute firewall-rules create fw-default-iapproxy \
 --network=default \
 --action=ALLOW \
 --rules=tcp:22,icmp \
---source-ranges=35.235.240.0/20 >/dev/null 2>&1 &
+--source-ranges=35.235.240.0/20 >/dev/null 2>&1
 
 gcloud compute firewall-rules create allow-http-traffic \
 --direction=INGRESS \
@@ -39,11 +39,11 @@ gcloud compute firewall-rules create allow-http-traffic \
 --action=ALLOW \
 --rules=tcp:80 \
 --source-ranges=0.0.0.0/0 \
---target-tags=http-server >/dev/null 2>&1 &
+--target-tags=http-server >/dev/null 2>&1
 
-gcloud compute instances create us-client-vm --machine-type e2-micro --zone $zone1 >/dev/null 2>&1 & 
-gcloud compute instances create europe-client-vm --machine-type e2-micro --zone $zone2 >/dev/null 2>&1 & 
-gcloud compute instances create asia-client-vm --machine-type e2-micro --zone $zone3 >/dev/null 2>&1 & 
+gcloud compute instances create us-client-vm --machine-type e2-micro --zone $zone1 >/dev/null 2>&1 
+gcloud compute instances create europe-client-vm --machine-type e2-micro --zone $zone2 >/dev/null 2>&1 
+gcloud compute instances create asia-client-vm --machine-type e2-micro --zone $zone3 >/dev/null 2>&1
 
 gcloud compute instances create us-web-vm \
 --zone=$zone1 \
@@ -54,8 +54,8 @@ gcloud compute instances create us-web-vm \
 --metadata=startup-script="#! /bin/bash
 apt-get update -y
 apt-get install apache2 -y
-echo 'Page served from: $region1 - Dr Abhishek' > /var/www/html/index.html
-systemctl restart apache2" >/dev/null 2>&1 &
+echo '' > /var/www/html/index.html
+systemctl restart apache2" >/dev/null 2>&1
 
 gcloud compute instances create europe-web-vm \
 --zone=$zone2 \
@@ -66,8 +66,8 @@ gcloud compute instances create europe-web-vm \
 --metadata=startup-script="#! /bin/bash
 apt-get update -y
 apt-get install apache2 -y
-echo 'Page served from: $region2 - Dr Abhishek' > /var/www/html/index.html
-systemctl restart apache2" >/dev/null 2>&1 &
+echo '' > /var/www/html/index.html
+systemctl restart apache2" >/dev/null 2>&1
 
 US_WEB_IP=$(gcloud compute instances describe us-web-vm --zone=$zone1 --format="value(networkInterfaces.networkIP)")
 EUROPE_WEB_IP=$(gcloud compute instances describe europe-web-vm --zone=$zone2 --format="value(networkInterfaces.networkIP)")
@@ -76,14 +76,14 @@ gcloud dns managed-zones create example \
 --description=test \
 --dns-name=example.com \
 --networks=default \
---visibility=private >/dev/null 2>&1 &
+--visibility=private >/dev/null 2>&1
 
 gcloud dns record-sets create geo.example.com \
 --ttl=5 \
 --type=A \
 --zone=example \
 --routing-policy-type=GEO \
---routing-policy-data="$region1=$US_WEB_IP;$region2=$EUROPE_WEB_IP" >/dev/null 2>&1 &
+--routing-policy-data="$region1=$US_WEB_IP;$region2=$EUROPE_WEB_IP" >/dev/null 2>&1
 pattern=(
 "**********************************************************"
 "**                 S U B S C R I B E  TO                **"
